6b4da62321d47141ea92c32d477140be681e321c
Hooamn_Sarv <hoomansarvghaddiiut@gmail.com>
2020-07-08 17:25:37 +0430

    fix

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../actions';
+
+function HomePage() {
+    const users = useSelector(state => state.users);
+    const user = useSelector(state => state.authentication.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.getAll());
+    }, []);
+
+    function handleDeleteUser(id) {
+        dispatch(userActions.delete(id));
+    }
+
+    return (
+        <div className="col-lg-8 ">
+            <h1>Hi {user.firstName}!</h1>
+            <p>You're logged in !!</p>
+            <h3>All registered users:</h3>
+            {users.loading && <em>Loading users...</em>}
+            {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+            {users.items &&
+                <ul>
+                    {users.items.map((user, index) =>
+                        <li key={user.id}>
+                            {user.firstName + ' ' + user.lastName}
+                        </li>
+                    )}
+                </ul>
+            }
+            <p>
+                <Link to="/login">Logout</Link>
+            </p>
+        </div>
+    );
+}
+
+export { HomePage };
